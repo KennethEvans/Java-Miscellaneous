92ad0e69ab280aeb13df0c11cc027a2055e566b1
Kenneth Evans Jr <ken@kenevans.net>
2015-09-23 18:35:23 +0000

    Modified Test.java to do check some dates.
    git-svn-id: http://svn.kenevans.net/Sandbox/trunk@799 4b487630-4e8c-464e-b16a-428ddfb157b8

--- src/misc/Test.java
+++ src/misc/Test.java
@@ -1,5 +1,7 @@
 package misc;
 
+import java.util.Date;
+
 /*
  * Created on Dec 24, 2011
  * By Kenneth Evans, Jr.
@@ -12,16 +14,23 @@ public class Test
      * @param args
      */
     public static void main(String[] args) {
-        String string = "abc, def";
-        int len = string.length();
-        int index = string.indexOf(",");
-        String string1 = string.substring(index+1, len);
-        String string3 = string.substring(0,index);
-        string1 = string1.trim();
-        string3 = string3.trim();
-        System.out.println("string: " + string);
-        System.out.println("string1: " + string1);
-        System.out.println("string3: " + string3);
+        // String string = "abc, def";
+        // int len = string.length();
+        // int index = string.indexOf(",");
+        // String string1 = string.substring(index+1, len);
+        // String string3 = string.substring(0,index);
+        // string1 = string1.trim();
+        // string3 = string3.trim();
+        // System.out.println("string: " + string);
+        // System.out.println("string1: " + string1);
+        // System.out.println("string3: " + string3);
+
+        // Check date for Infinite Painter
+        long lval1 = 1442603822783l;
+        Date date = new Date(lval1);
+        System.out.println(date + "  " + date.getTime());
+        date = new Date();
+        System.out.println(date + "  " + date.getTime());
     }
 
 }
