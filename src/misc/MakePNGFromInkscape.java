9baf6a60246e3c00f74c2a6e2c0f32192015899c
Kenneth Evans <ken@kenevans.net>
2017-02-22 01:02:20 -0500

    Added MakePNGFromInkscape.java.

---
+++ src/misc/MakePNGFromInkscape.java
@@ -0,0 +1,154 @@
+package misc;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+
+import run.Run;
+
+/**
+ * Converts a SVG file in Inkscape to PNG files of differing resolutions.
+ * Overwrites files in the destination.
+ * 
+ * @author Kenneth Evans, Jr.
+ */
+public class MakePNGFromInkscape
+{
+    private static final String SVG_NAME = "Windows Performance.svg";
+    private static final String DEST_BASE_NAME = "WindowsPerformance";
+    private static final int[] SIZES = {16, 32, 48, 96, 256};
+    // private static final int[] SIZES = {32};
+    private static final String SVG_PATH = "C:/Users/evans/Documents/Inkscape";
+    private static final String DEST_PATH = "C:/Scratch/Icons";
+    private static final String EXE_PATH = "C:/Program Files/Inkscape/inkscape.exe";
+
+    private static final String LS = Utils.LS;
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat(
+        "MM/dd/yyyy h:mm:ss a");
+
+    /**
+     * Generic method to run a command in a given directory, and capture the
+     * output. Uses the current environment, and default timeout.
+     * 
+     * @param cmdArray
+     * @param dir
+     * @return
+     * @throws IOException
+     */
+    public static String run(String[] cmdArray, File dir) throws IOException {
+        String out = "";
+        Run run = new Run();
+        // Need to set useBuffer to get the output
+        int retCode = run.run(cmdArray, null, dir, true);
+
+        if(retCode != 0) {
+            out += "The return code was: " + retCode + LS;
+        }
+
+        // Print output
+        String output = run.getOutput();
+        if(output != null) {
+            out += output;
+        }
+
+        // Print error output
+        String errOutput = run.getError();
+        if(errOutput != null && errOutput.length() > 0) {
+            out += "Error output: + LS";
+            out += errOutput;
+        }
+
+        Process proc = run.getProc();
+        if(proc != null) {
+            proc.destroy();
+        }
+
+        return out;
+    }
+
+    private static void processExport(int size) {
+        ArrayList<String> argsList = new ArrayList<String>();
+        File svgFile;
+        svgFile = new File(SVG_PATH, SVG_NAME);
+        argsList.add(EXE_PATH);
+        argsList.add("--without-gui");
+        argsList.add("--file=" + svgFile.getPath());
+        argsList.add("--export-png=" + DEST_BASE_NAME + "." + size + "x" + size
+            + ".png");
+        argsList.add("--export-width=" + size);
+        argsList.add("--export-height=" + size);
+        // argsList.add("--verb=FileClose");
+        // argsList.add("--verb=FileQuit");
+
+        String[] cmds = argsList.toArray(new String[argsList.size()]);
+
+        System.out.println("Running:");
+        for(String line : cmds) {
+            System.out.println(line);
+        }
+        System.out.println();
+
+        try {
+            run(cmds, new File(DEST_PATH));
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Lists the files in the given path.
+     */
+    private static void listDir() {
+        // Get the current files in the directory
+        System.out.println("Currently in " + DEST_PATH + ":");
+        File outDir = new File(DEST_PATH);
+        File[] dirs = outDir.listFiles(new FileFilter() {
+            public boolean accept(File file) {
+                if(file.getName().startsWith(DEST_BASE_NAME)) {
+                    return true;
+                }
+                return false;
+            }
+        });
+        Arrays.sort(dirs, new Comparator<File>() {
+            public int compare(File f1, File f2) {
+                long mod1 = f1.lastModified();
+                long mod2 = f2.lastModified();
+                if(mod1 > mod2) return 1;
+                if(mod1 < mod2) return -1;
+                return 0;
+            }
+        });
+        for(File file : dirs) {
+            System.out.printf("%5.0f KB  %s  %s" + LS, file.length() / 1024.,
+                dateFormat.format(new Date(file.lastModified())),
+                file.getName());
+        }
+
+        String[] cmds = new String[] {"ls"};
+        try {
+            run(cmds, new File(DEST_PATH));
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        for(int size : SIZES) {
+            processExport(size);
+        }
+
+        // Get the current files in the directory
+        listDir();
+
+        System.out.println();
+        System.out.println("All Done");
+        System.exit(0);
+    }
+
+}
