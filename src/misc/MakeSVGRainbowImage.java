fe18c20e4a9d52b7835e3fd29639b8b70fb3ea44
Kenneth Evans <ken@kenevans.net>
2018-12-11 12:03:13 -0500

    Fixed up MakeSVGRainbowImage.

--- src/misc/MakeSVGRainbowImage.java
+++ src/misc/MakeSVGRainbowImage.java
@@ -6,26 +6,30 @@ public class MakeSVGRainbowImage
 {
     private static final int NCOLORS = 16;
     private static int nColors = NCOLORS;
-    
-    private static final String[] PRE = {
-//        "<?xml version=\"1.0\" standalone=\"no\"?>",
-//        "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"",
-//        "  \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">",
-//        "<svg width=\"8cm\" height=\"4cm\" viewBox=\"0 0 800 400\" version=\"1.1\"",
-//        "     xmlns=\"http://www.w3.org/2000/svg\">",
-//        "  <desc>Rainbow Gradient</desc>", "  <g>", "    <defs>",
-//        "      <linearGradient id=\"MyGradient\">",
-//        // End
+
+    private static final String[] PRE = {"<svg>",
+        "    <linearGradient id=\"Rainbow Colors\" gradientUnits=\"objectBoundingBox\" spreadMethod=\"pad\" >",
+        // "<?xml version=\"1.0\" standalone=\"no\"?>",
+        // "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"",
+        // " \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">",
+        // "<svg width=\"8cm\" height=\"4cm\" viewBox=\"0 0 800 400\"
+        // version=\"1.1\"",
+        // " xmlns=\"http://www.w3.org/2000/svg\">",
+        // " <desc>Rainbow Gradient</desc>", " <g>", " <defs>",
+        // " <linearGradient id=\"MyGradient\">",
+        // End
     };
-    private static final String[] POST = {"      </linearGradient>",
-//        "    </defs>", "", "    <!-- Outline the drawing area in black -->",
-//        "    <rect fill=\"none\" stroke=\"black\" ",
-//        "          x=\"1\" y=\"1\" width=\"798\" height=\"398\"/>", "",
-//
-//        "    <!-- The rectangle is filled using a linear gradient paint server -->",
-//        "    <rect fill=\"url(#MyGradient)\" stroke=\"black\" stroke-width=\"5\"  ",
-//        "          x=\"100\" y=\"100\" width=\"600\" height=\"200\"/>",
-//        "  </g>", "</svg>",
+    private static final String[] POST = {"      </linearGradient>", "</svg>",
+        // " </defs>", "", " <!-- Outline the drawing area in black -->",
+        // " <rect fill=\"none\" stroke=\"black\" ",
+        // " x=\"1\" y=\"1\" width=\"798\" height=\"398\"/>", "",
+        //
+        // " <!-- The rectangle is filled using a linear gradient paint server
+        // -->",
+        // " <rect fill=\"url(#MyGradient)\" stroke=\"black\" stroke-width=\"5\"
+        // ",
+        // " x=\"100\" y=\"100\" width=\"600\" height=\"200\"/>",
+        // " </g>", "</svg>",
         // End
     };
 
@@ -50,7 +54,8 @@ public class MakeSVGRainbowImage
             offset = String.format("%5.3f", (double)i / (nColors - 1));
             System.out
                 .println("        <stop offset=\"" + offset + "\" stop-color=\""
-                    + RainbowColorScheme.toColorHexString(color) + "\" />");
+                    + RainbowColorScheme.toColorHexString(color) + "\""
+                    + " stop-opacity=\"1\"" + "/>");
         }
 
         // Start
